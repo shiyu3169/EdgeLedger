a8599f31bc65fcd86752805e62931aedc81edffc
Shiyu Wang <shiyu3169@gmail.com>
2019-03-25 14:06:40 -0400

    opacity and animaion

--- js/main.js
+++ js/main.js
@@ -13,3 +13,12 @@ function initMap() {
 //     zoom: 8
 //   });
 // }
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
